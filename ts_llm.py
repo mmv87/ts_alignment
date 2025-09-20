b251acc6626ca53a83a9fdc90a2ddbd902433cff
Mithun <mithun.kct@gmail.com>
2025-09-20 19:31:16 -0400

    epoch_changed

--- ts_llm.py
+++ ts_llm.py
@@ -40,7 +40,7 @@ def dataset_align(file):
     list_data=[]
     with open(file,'r',encoding='utf-8') as file:
         for idx,line in enumerate(file):
-            if idx<2500:
+            if idx<5500:
                 obj= json.loads(line)
                 list_data.append(obj)
             else:
